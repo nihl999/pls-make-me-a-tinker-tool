8eb5bea0fc3c00ea7c588e229fe1f4329bee5017
Arthur Lorhan <arthurlohransilva@gmail.com>
2020-09-05 04:53:46 -0300

    knex configuration

--- src/controllers/ToolBuilder.ts
+++ src/controllers/ToolBuilder.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
+import knex from '../config/knexconfig';
 
 export default (_req: Request, res: Response) => {
-  res.send('<h1>Hello World!</h1>');
+    console.log(knex);
+    res.send('<h1>Hello World!</h1>');
 };
